e67199dadd10c2545efb54b105a9050d6539f08d
AdrisGithub <adrianjohn6543@gmail.com>
2025-05-06 14:35:27 +0200

    ✨ implement equals, hashCode and toString for Failure

--- src/main/java/io/github/adrisgithub/result/FailureResult.java
+++ src/main/java/io/github/adrisgithub/result/FailureResult.java
@@ -1,5 +1,6 @@
 package io.github.adrisgithub.result;
 
+import java.util.Objects;
 import java.util.Optional;
 
 public final class FailureResult<T, E extends RuntimeException> implements Result<T, E> {
@@ -23,5 +24,25 @@ public final class FailureResult<T, E extends RuntimeException> implements Resul
   @Override
   public Optional<T> getValue() {
     return Optional.empty();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof FailureResult<?, ?> that)) {
+      return false;
+    }
+    return Objects.equals(getError(), that.getError());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(exception);
+  }
+
+  @Override
+  public String toString() {
+    return "Result[" +
+        "exception=" + exception +
+        ']';
   }
 }
